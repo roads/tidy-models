b5773449c77c73537aaea7ae5638feea9562b86c
roads <brett.roads@gmail.com>
2023-01-04 16:20:03 +0000

    Update package setup and organization.

--- setup.py
+++ setup.py
@@ -1,26 +1,11 @@
-"""Setup file."""
+#!/usr/bin/env python
+"""Setup file.
+
+Retained for legacy compatibility and installing via editable mode.
+
+"""
+
 import setuptools
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
-setuptools.setup(
-    name='tidy_models',
-    version='0.1.0',
-    description='Boilerplate for keeping model results tidy.',
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    classifiers=[
-        'Programming Language :: Python :: 3',
-    ],
-    author='Brett D. Roads',
-    author_email='brett.roads@gmail.com',
-    license='Apache Licence 2.0',
-    packages=['tidy_models'],
-    python_requires='>=3.5, <3.10',
-    install_requires=[
-        'pandas'
-    ],
-    include_package_data=True,
-    url='https://github.com/roads/tidy-models'
-)
+if __name__ == "__main__":
+    setuptools.setup()
